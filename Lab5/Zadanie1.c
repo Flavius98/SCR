8202edb027ab47f17b62a6c269a79274e257b11e
Flavius98 <62777214+Flavius98@users.noreply.github.com>
2020-12-02 20:29:34 +0100

    Add files via upload

--- Lab5/Zadanie1.c
+++ Lab5/Zadanie1.c
@@ -11,7 +11,6 @@ int main(int argc , char* argv[])
     int fileDescNum[2];
 
     pipe(fileDescNum);
-
     pidNum = fork();
 
     if (pidNum)
@@ -21,9 +20,10 @@ int main(int argc , char* argv[])
         while (read(fileDescNum[0], bufor, MAX_BUFFOR))
         {
             if (bufor[MAX_BUFFOR - 1] == 0)
-               
+                break;
         
-               
+                bufor[MAX_BUFFOR - 1] = '\0';
+                printf("#%s#\n", bufor);
         }
     }
 
@@ -32,7 +32,14 @@ int main(int argc , char* argv[])
         FILE *f = fopen(argv[1],"r");
         char buffor[MAX_BUFFOR];
 
-        
+        while (fgets(buffor, MAX_BUFFOR, f))
+        {
+            buffor[MAX_BUFFOR-1] = 1;
+            write(fileDescNum[1],buffor,MAX_BUFFOR);
+        }
+
+        buffor[MAX_BUFFOR-1] = 0;
+        write(fileDescNumb[1], buffor, MAX_BUFFOR);
 
         fclose(f);  
     }
