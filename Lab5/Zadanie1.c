16fd4477726a1ba51a6b4296c09e14017f1b8dc7
Flavius98 <62777214+Flavius98@users.noreply.github.com>
2020-12-02 20:01:32 +0100

    Add files via upload

---
+++ Lab5/Zadanie1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#define MAX_BUFFOR 64
+
+
+
+int main(int argc , char* argv[])
+{
+
+int pidNum;
+int fileDescNumb[2];
+
+pipe(fileDescNumb);
+
+pidNumb = fork();
+
+if (pidNumb)
+{
+
+   
+}
+else
+{
+ 
+}
+
+
+
+}
